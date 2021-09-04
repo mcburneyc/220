d00a0c22a2c7074ca72fb6d23a49b705ade1d820
mcburneyc <mcburneyc@g.cofc.edu>
2021-09-04 12:08:33 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: Cooper McBurney
+hello.py
+
+Problem: This Program will show how to write, test, and submit a function.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+main()
