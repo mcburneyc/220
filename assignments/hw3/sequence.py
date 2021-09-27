2d75f9f06c05fef5129003635ac22b42632c21b9
mcburneyc <mcburneyc@g.cofc.edu>
2021-09-27 13:49:51 -0400

    completed hw1

---
+++ assignments/hw3/sequence.py
@@ -0,0 +1,12 @@
+"""
+Name: Cooper McBurney
+sequence.py
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def sequence():
+    number_of_terms = eval(input("enter the number of terms:"))
+    for i in range(number_of_terms):
+        print(i, end=" ")
